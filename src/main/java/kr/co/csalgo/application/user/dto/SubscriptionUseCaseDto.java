8f7f2e6de44c80af8569c2db69cfaa4ed6deb780
wlstj <22020306jinseoyeon@gmail.com>
2025-05-16 11:03:09 +0900

    refactor: @Getter 애노테이션으로 변경 #8

--- src/main/java/kr/co/csalgo/application/user/dto/SubscriptionUseCaseDto.java
+++ src/main/java/kr/co/csalgo/application/user/dto/SubscriptionUseCaseDto.java
@@ -4,11 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import kr.co.csalgo.domain.user.entity.User;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 
 public class SubscriptionUseCaseDto {
 
-    @Data
+    @Getter
     public static class Request {
         @NotBlank(message = "이메일은 필수 입력 값입니다.")
         @Email(message = "이메일 형식이 올바르지 않습니다.")
@@ -20,7 +20,7 @@ public class SubscriptionUseCaseDto {
         }
     }
 
-    @Data
+    @Getter
     public static class Response {
         private Long subscriptionId;
 
