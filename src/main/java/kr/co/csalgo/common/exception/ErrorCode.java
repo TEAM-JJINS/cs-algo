126532c48dfbac35382d50bb59345dc56f023b28
Jinwoo Lee <jinlee1703@gmail.com>
2025-05-20 20:51:12 +0900

    feat: 공통 예외 처리를 위한 에러 코드 정의 #23
    Co-authored-by: 진서연 <22020306jinseoyeon@gmail.com>

--- src/main/java/kr/co/csalgo/common/exception/ErrorCode.java
+++ src/main/java/kr/co/csalgo/common/exception/ErrorCode.java
@@ -1,17 +1,22 @@
 package kr.co.csalgo.common.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum ErrorCode {
+    // A: 일반 요청 오류
+    INVALID_INPUT("A001", "잘못된 입력값입니다."),
+    MESSGAE_NOT_READABLE("A002", "요청 본문을 읽을 수 없습니다.."),
 
-    DUPLICATE_EMAIL("이메일이 이미 등록되어 있습니다.");
+    // B: 비즈니스 로직 오류
+    USER_NOT_FOUND("B001", "사용자를 찾을 수 없습니다."),
+    DUPLICATE_EMAIL("B002", "이메일이 이미 등록되어 있습니다."),
 
+    // Z: 시스템 오류
+    INTERNAL_SERVER_ERROR("Z001", "서버 내부가 발생했습니다.");
 
+    private final String code;
     private final String message;
-
-    ErrorCode(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
