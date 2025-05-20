70e372eb0dc66160e2c076735b8bfb5babd1bb0a
wlstj <22020306jinseoyeon@gmail.com>
2025-05-20 14:28:04 +0900

    refactor: 생성자 애노테이션으로 대체 #18

--- src/main/java/kr/co/csalgo/domain/auth/repository/ConcurrentMapVerificationCodeRepository.java
+++ src/main/java/kr/co/csalgo/domain/auth/repository/ConcurrentMapVerificationCodeRepository.java
@@ -1,21 +1,19 @@
 package kr.co.csalgo.domain.auth.repository;
 
 import kr.co.csalgo.domain.auth.type.VerificationCodeType;
+import lombok.RequiredArgsConstructor;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+@RequiredArgsConstructor
 public class ConcurrentMapVerificationCodeRepository implements VerificationCodeRepository {
 
     private final ConcurrentHashMap<String, String> verificationCodeMap = new ConcurrentHashMap<>();
     private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
     private final long expireTime;
-
-    public ConcurrentMapVerificationCodeRepository(long expireTime) {
-        this.expireTime = expireTime;
-    }
 
     @Override
     public void create(String email, String code, VerificationCodeType verificationCodeType) {
