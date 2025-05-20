c8538dc877ffb99458029c7e0b2f739b8b135d34
wlstj <22020306jinseoyeon@gmail.com>
2025-05-20 23:47:14 +0900

    docs: EmailVerificationController api 문서화 #30

--- src/main/java/kr/co/csalgo/presentation/auth/EmailVerificationController.java
+++ src/main/java/kr/co/csalgo/presentation/auth/EmailVerificationController.java
@@ -1,5 +1,9 @@
 package kr.co.csalgo.presentation.auth;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.co.csalgo.application.auth.dto.EmailVerificationCodeDto;
 import kr.co.csalgo.application.auth.usecase.EmailVerificationUseCase;
@@ -13,10 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth/email-verifications")
+@Tag(name = "Email Verification", description = "이메일 인증코드 관련 API")
 public class EmailVerificationController {
     private final EmailVerificationUseCase emailVerificationUseCase;
 
     @PostMapping("/request")
+    @Operation(summary = "인증코드 요청", description = "사용자가 이메일을 통해 인증코드를 요청 및 수신합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "인증코드 전송 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 (유효성 검사 실패)"),
+    })
     public ResponseEntity<?> sendEmailVerificationCode(@Valid @RequestBody EmailVerificationCodeDto.Request request) {
         return ResponseEntity.ok(emailVerificationUseCase.sendEmailVerificationCode(request));
     }
