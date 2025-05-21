b11fe841fa87f651164a863f26a4f90947521cf5
Jinwoo Lee <jinlee1703@gmail.com>
2025-05-21 14:19:50 +0900

    fix: id 필드 중복 선언 제거 #24

--- src/main/java/kr/co/csalgo/domain/common/entity/AuditableEntity.java
+++ src/main/java/kr/co/csalgo/domain/common/entity/AuditableEntity.java
@@ -1,6 +1,8 @@
 package kr.co.csalgo.domain.common.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -12,10 +14,6 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AuditableEntity extends IdentifiableEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     @CreatedDate
     @Column(nullable = false)
     private LocalDateTime createdAt;
