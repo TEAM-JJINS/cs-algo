c46d09b499ce886433c35f6ae00e796aa3f4e696
wlstj <22020306jinseoyeon@gmail.com>
2025-05-22 22:51:48 +0900

    chore: 패키지명 변경 #22

--- src/main/java/kr/co/csalgo/web/unsubscribe/UnSubscribeWebController.java
+++ src/main/java/kr/co/csalgo/web/unsubscribe/UnSubscribeWebController.java
@@ -1,4 +1,4 @@
-package kr.co.csalgo.web.subscribe;
+package kr.co.csalgo.web.unsubscribe;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
