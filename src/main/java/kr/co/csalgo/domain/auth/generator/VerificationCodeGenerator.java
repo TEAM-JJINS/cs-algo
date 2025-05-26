4e88b36bded014479fb972128f951c4ee543f8f3
Jinwoo Lee <jinlee1703@gmail.com>
2025-05-26 22:57:54 +0900

    refactor: 인증코드 생성기 컴포넌트 설정 및 인터페이스 분리 #19

---
+++ src/main/java/kr/co/csalgo/domain/auth/generator/VerificationCodeGenerator.java
@@ -0,0 +1,5 @@
+package kr.co.csalgo.domain.auth.generator;
+
+public interface VerificationCodeGenerator {
+	String generate();
+}
--- src/main/java/kr/co/csalgo/domain/auth/service/VerificationCodeService.java
+++ src/main/java/kr/co/csalgo/domain/auth/service/VerificationCodeService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 import kr.co.csalgo.common.exception.CustomBusinessException;
 import kr.co.csalgo.common.exception.ErrorCode;
-import kr.co.csalgo.common.util.VerificationCodeUtil;
+import kr.co.csalgo.domain.auth.generator.VerificationCodeGenerator;
 import kr.co.csalgo.domain.auth.repository.VerificationCodeRepository;
 import kr.co.csalgo.domain.auth.type.VerificationCodeType;
 import lombok.RequiredArgsConstructor;
@@ -13,23 +13,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class VerificationCodeService {
 	private final VerificationCodeRepository verificationCodeRepository;
+	private final VerificationCodeGenerator secureVerificationCodeGenerator;
 
 	public String create(String email, VerificationCodeType verificationCodeType) {
-		String code = VerificationCodeUtil.generate();
+		String code = secureVerificationCodeGenerator.generate();
 		saveVerificationCode(email, code, verificationCodeType);
 		return code;
 	}
 
 	public boolean verify(String email, String code, VerificationCodeType verificationCodeType) {
-		try {
-			boolean isValid = verificationCodeRepository.verify(email, code, verificationCodeType);
-			if (!isValid) {
-				throw new CustomBusinessException(ErrorCode.VERIFICATION_CODE_MISMATCH);
-			}
-			return true;
-		} catch (Exception e) {
-			throw new CustomBusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+		boolean isValid = verificationCodeRepository.verify(email, code, verificationCodeType);
+		if (!isValid) {
+			throw new CustomBusinessException(ErrorCode.VERIFICATION_CODE_MISMATCH);
 		}
+		return true;
 	}
 
 	private void saveVerificationCode(String email, String code, VerificationCodeType verificationCodeType) {
