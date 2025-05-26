5633b6560f896dc2d9177761cc0917343f9eafac
Jinwoo Lee <jinlee1703@gmail.com>
2025-05-26 21:26:57 +0900

    test: 이메일 인증 코드 검증 API 테스트 작성 #19

--- src/test/java/kr/co/csalgo/presentation/auth/EmailVerificationControllerTest.java
+++ src/test/java/kr/co/csalgo/presentation/auth/EmailVerificationControllerTest.java
@@ -107,4 +107,104 @@ public class EmailVerificationControllerTest {
 			.andExpect(status().isBadRequest())
 			.andDo(print());
 	}
+
+	@Test
+	@DisplayName("이메일 검증 코드를 입력하여 인증을 완료할 수 있다.")
+	void testVerifyEmailCodeSuccess() throws Exception {
+		EmailVerficationVerifyDto.Request request = EmailVerficationVerifyDto.Request.builder()
+			.email("team.jjins@gmail.com")
+			.code("123456")
+			.type(VerificationCodeType.SUBSCRIPTION)
+			.build();
+
+		mockMvc.perform(post("/api/auth/email-verifications/verify")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(request)))
+			.andExpect(status().isOk())
+			.andDo(print());
+	}
+
+	@Test
+	@DisplayName("이메일 검증 코드 입력 시 이메일이 공백이면 인증을 완료할 수 없다.")
+	void testVerifyFailWhenEmailIsBlank() throws Exception {
+		EmailVerficationVerifyDto.Request request = EmailVerficationVerifyDto.Request.builder()
+			.email("")
+			.code("123456")
+			.type(VerificationCodeType.SUBSCRIPTION)
+			.build();
+
+		mockMvc.perform(post("/api/auth/email-verifications/verify")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(request)))
+			.andExpect(status().isBadRequest())
+			.andDo(print());
+	}
+
+	@Test
+	@DisplayName("이메일 검증 코드 입력 시 코드가 공백이면 인증을 완료할 수 없다.")
+	void testVerifyFailWhenCodeIsBlank() throws Exception {
+		EmailVerficationVerifyDto.Request request = EmailVerficationVerifyDto.Request.builder()
+			.email("team.jjins@gmail.com")
+			.code("")
+			.type(VerificationCodeType.SUBSCRIPTION)
+			.build();
+
+		mockMvc.perform(post("/api/auth/email-verifications/verify")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(request)))
+			.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	@DisplayName("이메일 검증 코드 입력 시 인증 type이 공백이면 인증을 완료할 수 없다.")
+	void testVerifyFailWhenTypeIsBlank() throws Exception {
+		EmailVerficationVerifyDto.Request request = EmailVerficationVerifyDto.Request.builder()
+			.email("")
+			.code("123456")
+			.type(VerificationCodeType.SUBSCRIPTION)
+			.build();
+
+		mockMvc.perform(post("/api/auth/email-verifications/verify")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(request)))
+			.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	@DisplayName("올바르지 않은 이메일 형식으로는 인증번호를 받을 수 없다.")
+	void testVerifyFailWhenEmailFormatIsInvalid() throws Exception {
+		EmailVerficationVerifyDto.Request request = EmailVerficationVerifyDto.Request.builder()
+			.email("team-jjinsgmail.com")
+			.code("123456")
+			.type(VerificationCodeType.SUBSCRIPTION)
+			.build();
+
+		mockMvc.perform(post("/api/auth/email-verifications/request")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(request)))
+			.andExpect(status().isBadRequest())
+			.andDo(print());
+	}
+
+	@Test
+	@DisplayName("사용자는 이미 검증된 이메일로 재검증을 받을 수 없다.")
+	void testVerifyFailWhenResend() throws Exception {
+		EmailVerficationVerifyDto.Request request = EmailVerficationVerifyDto.Request.builder()
+			.email("team.jjins@gmail.com")
+			.code("123456")
+			.type(VerificationCodeType.SUBSCRIPTION)
+			.build();
+
+		mockMvc.perform(post("/api/auth/email-verifications/verify")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(request)))
+			.andExpect(status().isOk())
+			.andDo(print());
+
+		mockMvc.perform(post("/api/auth/email-verifications/verify")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(request)))
+			.andExpect(status().isBadRequest())
+			.andDo(print());
+	}
 }
