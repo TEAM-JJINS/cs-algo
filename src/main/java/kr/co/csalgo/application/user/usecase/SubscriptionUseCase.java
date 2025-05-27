9f865309d3568575171f55a1b33c7bd0eb76515b
Jinwoo Lee <jinlee1703@gmail.com>
2025-05-28 01:19:14 +0900

    feat: 구독 UseCase 로그 추가 #42

--- src/main/java/kr/co/csalgo/application/user/usecase/SubscriptionUseCase.java
+++ src/main/java/kr/co/csalgo/application/user/usecase/SubscriptionUseCase.java
@@ -7,20 +7,32 @@ import kr.co.csalgo.application.user.dto.UnsubscriptionUseCaseDto;
 import kr.co.csalgo.domain.user.entity.User;
 import kr.co.csalgo.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SubscriptionUseCase {
 
 	private final UserService userService;
 
 	public SubscriptionUseCaseDto.Response create(SubscriptionUseCaseDto.Request request) {
+		log.info("[구독 요청] email={}", request.getEmail());
+
 		User user = userService.create(request.getEmail());
+
+		log.info("[구독 완료] userId={}, email={}", user.getId(), user.getEmail());
+
 		return SubscriptionUseCaseDto.Response.of();
 	}
 
 	public UnsubscriptionUseCaseDto.Response unsubscribe(UnsubscriptionUseCaseDto.Request request) {
+		log.info("[구독 해지 요청] userId={}", request.getUserId());
+
 		userService.delete(request.getUserId());
+
+		log.info("[구독 해지 완료] userId={}", request.getUserId());
+
 		return UnsubscriptionUseCaseDto.Response.of();
 	}
 }
