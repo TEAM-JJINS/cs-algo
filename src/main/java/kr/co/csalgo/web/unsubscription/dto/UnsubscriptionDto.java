f99971ca2ca13fe333779fc566dbc7161788a8df
wlstj <22020306jinseoyeon@gmail.com>
2025-05-30 15:59:50 +0900

    refactor: dto request 제거 #44

--- src/main/java/kr/co/csalgo/web/unsubscription/dto/UnsubscriptionDto.java
+++ src/main/java/kr/co/csalgo/web/unsubscription/dto/UnsubscriptionDto.java
@@ -3,17 +3,8 @@ package kr.co.csalgo.web.unsubscription.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 public class UnsubscriptionDto {
-
-	@Getter
-	@Setter
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public static class Request {
-		private Long userId;
-	}
 
 	@Getter
 	@NoArgsConstructor
