1c84e845b5ae7745ed2232dfee5e036acb0f84e6
Jinwoo Lee <jinlee1703@gmail.com>
2025-06-04 03:17:07 +0900

    fix: 문제 존재 여부 기능 삭제 및 조회 기능 추가 #63

--- src/main/java/kr/co/csalgo/domain/question/repository/QuestionRepository.java
+++ src/main/java/kr/co/csalgo/domain/question/repository/QuestionRepository.java
@@ -1,9 +1,11 @@
 package kr.co.csalgo.domain.question.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.co.csalgo.domain.question.entity.Question;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-	boolean existsById(Long id);
+	Optional<Question> findById(Long id);
 }
--- src/main/java/kr/co/csalgo/domain/question/service/QuestionService.java
+++ src/main/java/kr/co/csalgo/domain/question/service/QuestionService.java
@@ -2,6 +2,9 @@ package kr.co.csalgo.domain.question.service;
 
 import org.springframework.stereotype.Service;
 
+import kr.co.csalgo.common.exception.CustomBusinessException;
+import kr.co.csalgo.common.exception.ErrorCode;
+import kr.co.csalgo.domain.question.entity.Question;
 import kr.co.csalgo.domain.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -10,7 +13,8 @@ import lombok.RequiredArgsConstructor;
 public class QuestionService {
 	private final QuestionRepository questionRepository;
 
-	public boolean isExists(Long id) {
-		return questionRepository.existsById(id);
+	public Question read(Long id) {
+		return questionRepository.findById(id)
+			.orElseThrow(() -> new CustomBusinessException(ErrorCode.QUESTION_NOT_FOUND));
 	}
 }
