fa5494421a08ee4b64621b4991265c88652e2551
wlstj <22020306jinseoyeon@gmail.com>
2025-06-11 19:39:22 +0900

    refactor: checkMailUseCase 제거 #72

--- src/main/java/kr/co/csalgo/presentation/scheduler/MailPollingScheduler.java
+++ src/main/java/kr/co/csalgo/presentation/scheduler/MailPollingScheduler.java
@@ -4,19 +4,16 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import jakarta.mail.MessagingException;
-import kr.co.csalgo.application.mail.usecase.CheckMailUseCase;
 import kr.co.csalgo.application.mail.usecase.RegisterQuestionResponseUseCase;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Component
 public class MailPollingScheduler {
-	private final CheckMailUseCase checkMailUseCase;
 	private final RegisterQuestionResponseUseCase registerQuestionResponseUseCase;
 
 	@Scheduled(cron = "0 */1 * * * *")
 	public void poll() throws MessagingException {
-		checkMailUseCase.size();
 		registerQuestionResponseUseCase.execute();
 	}
 }
--- src/test/java/kr/co/csalgo/presentation/scheduler/MailPollingSchedulerTest.java
+++ src/test/java/kr/co/csalgo/presentation/scheduler/MailPollingSchedulerTest.java
@@ -7,29 +7,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import jakarta.mail.MessagingException;
-import kr.co.csalgo.application.mail.usecase.CheckMailUseCase;
 import kr.co.csalgo.application.mail.usecase.RegisterQuestionResponseUseCase;
 
 class MailPollingSchedulerTest {
-	private CheckMailUseCase checkMailUseCase;
 	private RegisterQuestionResponseUseCase registerQuestionResponseUseCase;
 	private MailPollingScheduler mailPollingScheduler;
 
 	@BeforeEach
 	void setUp() {
-		checkMailUseCase = mock(CheckMailUseCase.class);
 		registerQuestionResponseUseCase = mock(RegisterQuestionResponseUseCase.class);
-		mailPollingScheduler = new MailPollingScheduler(checkMailUseCase, registerQuestionResponseUseCase);
+		mailPollingScheduler = new MailPollingScheduler(registerQuestionResponseUseCase);
 	}
 
 	@Test
-	@DisplayName("poll()이 호출되면 CheckMailUseCase.size()와 RegisterQuestionResponseUseCase.execute()가 실행되어야 한다")
+	@DisplayName("poll()이 호출되면 RegisterQuestionResponseUseCase.execute()가 실행되어야 한다")
 	void testPollSuccess() throws MessagingException {
 		// when
 		mailPollingScheduler.poll();
 
 		// then
-		verify(checkMailUseCase, times(1)).size();
 		verify(registerQuestionResponseUseCase, times(1)).execute();
 	}
 }
