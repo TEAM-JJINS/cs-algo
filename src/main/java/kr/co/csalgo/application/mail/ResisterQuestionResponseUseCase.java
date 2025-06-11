a6cfe7d2e8d4bb29b486a828c47b3bbc633bba02
wlstj <22020306jinseoyeon@gmail.com>
2025-06-11 13:57:11 +0900

    feat: question response 저장 로직 구현 #72

---
+++ src/main/java/kr/co/csalgo/application/mail/ResisterQuestionResponseUseCase.java
@@ -0,0 +1,10 @@
+package kr.co.csalgo.application.mail;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ResisterQuestionResponseUseCase {
+}
---
+++ src/main/java/kr/co/csalgo/domain/question/service/QuestionResponseService.java
@@ -0,0 +1,24 @@
+package kr.co.csalgo.domain.question.service;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.csalgo.domain.question.entity.Question;
+import kr.co.csalgo.domain.question.entity.QuestionResponse;
+import kr.co.csalgo.domain.question.repository.QuestionResponseRepository;
+import kr.co.csalgo.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionResponseService {
+	private final QuestionResponseRepository questionResponseRepository;
+
+	public void create(Question question, User user, String content) {
+		QuestionResponse questionResponse = QuestionResponse.builder()
+			.question(question)
+			.user(user)
+			.content(content)
+			.build();
+		questionResponseRepository.save(questionResponse);
+	}
+}
