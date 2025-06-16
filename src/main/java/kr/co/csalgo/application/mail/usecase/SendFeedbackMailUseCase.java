dc7020063df6b815e7534c3e715e87ab2cd21e73
Jinwoo Lee <jinlee1703@gmail.com>
2025-06-16 21:10:45 +0900

    feat: 피드백 메일 전송 유스케이스 초안 작성 #73

---
+++ src/main/java/kr/co/csalgo/application/mail/usecase/SendFeedbackMailUseCase.java
@@ -0,0 +1,15 @@
+package kr.co.csalgo.application.mail.usecase;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class SendFeedbackMailUseCase {
+	public void execute() {
+		// TODO: 1. 피드백이 전송되지 않은 문제 List 조회
+		// TODO: 2. 피드백이 전송되지 않은 문제에 대해 피드백 메일 전송
+		// TODO: 3. 피드백 메일 처리에 대한 로깅
+	}
+}
