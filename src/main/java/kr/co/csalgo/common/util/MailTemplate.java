c269c28441e55c54d21a441ced30ffd7e664f37c
Jinwoo Lee <jinlee1703@gmail.com>
2025-06-30 20:21:52 +0900

    refactor: 메일 템플릿 이미지 URL 분리 #100

--- src/main/java/kr/co/csalgo/common/util/MailTemplate.java
+++ src/main/java/kr/co/csalgo/common/util/MailTemplate.java
@@ -1,11 +1,23 @@
 package kr.co.csalgo.common.util;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class MailTemplate {
+
 	public static final String QUESTION_MAIL_SUBJECT = "[CS-ALGO] %s";
 	public static final String VERIFICATION_CODE_SUBJECT = "[CS-ALGO] 이메일 인증 코드";
 	public static final String FEEDBACK_MAIL_SUBJECT_REPLY = "Re: [CS-ALGO] %s";
+
+	private static final String LOGO_URL = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/logo.png";
+	private static final String QUESTION_ICON_URL = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/question.png";
+	private static final String RESPONSE_ICON_URL = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/response.png";
+	private static final String SOLUTION_ICON_URL = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/solution.png";
+	private static final String USAGE_ICON_URL = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/usage.png";
+	private static final String UNSUBSCRIPTION_ICON_URL = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/cancel.png";
+
+	private static final String PRIMARY_COLOR = "#1c1c1c";
+	private static final String BACKGROUND_COLOR = "#f6f6f6";
 
 	public static String formatVerificationCodeBody(String code) {
 		return wrapHtml(
@@ -48,7 +60,7 @@ public class MailTemplate {
 						a { text-decoration: none; color: inherit; }
 					</style>
 				</head>
-				<body style="margin:0; padding:0; background-color:#f6f6f6;" bgcolor="#f6f6f6">
+				<body style="margin:0; padding:0; background-color:%s;" bgcolor="%s">
 					<table align="center" width="100%%" cellpadding="0" cellspacing="0" style="padding:24px 0;">
 						<tr>
 							<td align="center">
@@ -62,18 +74,17 @@ public class MailTemplate {
 					</table>
 				</body>
 				</html>
-			""".formatted(bodyContent);
+			""".formatted(BACKGROUND_COLOR, BACKGROUND_COLOR, bodyContent);
 	}
 
 	private static String headerSection() {
-		String logoUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/logo.png";
 		return """
-				<tr><td style="background:#1c1c1c; padding:20px 24px;" bgcolor="#1c1c1c">
+				<tr><td style="background:%s; padding:20px 24px;" bgcolor="%s">
 					<a href="https://www.csalgo.co.kr" target="_blank">
 						<img src="%s" width="100" alt="CS-ALGO" style="display:block;">
 					</a>
 				</td></tr>
-			""".formatted(logoUrl);
+			""".formatted(PRIMARY_COLOR, PRIMARY_COLOR, LOGO_URL);
 	}
 
 	private static String verificationCodeSection(String code) {
@@ -102,13 +113,12 @@ public class MailTemplate {
 	}
 
 	private static String questionTitleSection(String questionTitle) {
-		String questionIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/question.png";
 		return """
 				<tr><td style="padding:0 24px;">
 					<img src="%s" width="20" style="vertical-align:middle; margin-right:6px;">
 					<span style="font-size:22px; font-weight:bold;">%s</span>
 				</td></tr>
-			""".formatted(questionIconUrl, escapeHtml(questionTitle));
+			""".formatted(QUESTION_ICON_URL, escapeHtml(questionTitle));
 	}
 
 	private static String instructionSection() {
@@ -120,8 +130,6 @@ public class MailTemplate {
 	}
 
 	private static String buttonSection(UUID userId) {
-		String usageIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/usage.png";
-		String unsubscriptionIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/cancel.png";
 		return """
 				<tr><td align="center" style="padding:36px 24px;">
 					<table cellpadding="0" cellspacing="0">
@@ -141,7 +149,7 @@ public class MailTemplate {
 						</tr>
 					</table>
 				</td></tr>
-			""".formatted(usageIconUrl, userId.toString(), unsubscriptionIconUrl);
+			""".formatted(USAGE_ICON_URL, userId, UNSUBSCRIPTION_ICON_URL);
 	}
 
 	private static String footerSection() {
@@ -153,7 +161,6 @@ public class MailTemplate {
 	}
 
 	private static String feedbackQuestionSection(String questionTitle) {
-		String questionIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/question.png";
 		return """
 				<tr><td style="padding:24px 24px 0;">
 					<img src="%s" width="20" style="vertical-align:middle; margin-right:6px;">
@@ -162,11 +169,10 @@ public class MailTemplate {
 				<tr><td style="padding:8px 24px 0; font-size:15px; color:#333; line-height:1.6;">
 					%s
 				</td></tr>
-			""".formatted(questionIconUrl, escapeHtml(questionTitle));
+			""".formatted(QUESTION_ICON_URL, escapeHtml(questionTitle));
 	}
 
 	private static String feedbackUserAnswerSection(String username, String userAnswer) {
-		String responseIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/response.png";
 		return """
 				<tr><td style="padding:32px 24px 0;">
 					<img src="%s" width="20" style="vertical-align:middle; margin-right:6px;">
@@ -175,11 +181,10 @@ public class MailTemplate {
 				<tr><td style="padding:8px 24px 0; font-size:15px; color:#333; line-height:1.6;">
 					%s
 				</td></tr>
-			""".formatted(responseIconUrl, escapeHtml(username), escapeHtml(userAnswer));
+			""".formatted(RESPONSE_ICON_URL, escapeHtml(username), escapeHtml(userAnswer));
 	}
 
 	private static String feedbackModelAnswerSection(String modelAnswer) {
-		String solutionIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/solution.png";
 		return """
 				<tr><td style="padding:32px 24px 0;">
 					<img src="%s" width="20" style="vertical-align:middle; margin-right:6px;">
@@ -188,14 +193,13 @@ public class MailTemplate {
 				<tr><td style="padding:8px 24px 36px; font-size:15px; color:#333; line-height:1.6;">
 					%s
 				</td></tr>
-			""".formatted(solutionIconUrl, escapeHtml(modelAnswer));
+			""".formatted(SOLUTION_ICON_URL, escapeHtml(modelAnswer));
 	}
 
 	private static String escapeHtml(String input) {
-		if (input == null) {
-			return "";
-		}
-		return input.replace("&", "&amp;")
+		// SonarCloud는 null check → Objects.toString(...) 권장
+		return Objects.toString(input, "")
+			.replace("&", "&amp;")
 			.replace("<", "&lt;")
 			.replace(">", "&gt;")
 			.replace("\"", "&quot;");
