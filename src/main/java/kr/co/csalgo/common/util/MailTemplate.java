9988ef9021bbabe4fa9a5c56e9be1e3e3ecd5c86
Jinwoo Lee <jinlee1703@gmail.com>
2025-06-30 20:05:16 +0900

    fix: 메일 템플릿 내 svg 파일을 s3 이미지 파일로 변경 #100

--- src/main/java/kr/co/csalgo/common/util/MailTemplate.java
+++ src/main/java/kr/co/csalgo/common/util/MailTemplate.java
@@ -1,16 +1,10 @@
 package kr.co.csalgo.common.util;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Base64;
 import java.util.UUID;
-
-import org.springframework.core.io.ClassPathResource;
 
 public class MailTemplate {
 	public static final String QUESTION_MAIL_SUBJECT = "[CS-ALGO] %s";
 	public static final String VERIFICATION_CODE_SUBJECT = "[CS-ALGO] 이메일 인증 코드";
-	public static final String VERIFICATION_CODE_BODY = "<h3>인증 코드</h3><p>%s</p>";
 	public static final String FEEDBACK_MAIL_SUBJECT_REPLY = "Re: [CS-ALGO] %s";
 
 	public static String formatVerificationCodeBody(String code) {
@@ -72,14 +66,14 @@ public class MailTemplate {
 	}
 
 	private static String headerSection() {
-		String logoBase64 = toBase64("static/images/logo.svg");
+		String logoUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/logo.png";
 		return """
 				<tr><td style="background:#1c1c1c; padding:20px 24px;" bgcolor="#1c1c1c">
 					<a href="https://www.csalgo.co.kr" target="_blank">
-						<img src="data:image/svg+xml;base64,%s" width="100" alt="CS-ALGO" style="display:block;">
+						<img src="%s" width="100" alt="CS-ALGO" style="display:block;">
 					</a>
 				</td></tr>
-			""".formatted(logoBase64);
+			""".formatted(logoUrl);
 	}
 
 	private static String verificationCodeSection(String code) {
@@ -108,13 +102,13 @@ public class MailTemplate {
 	}
 
 	private static String questionTitleSection(String questionTitle) {
-		String questionIcon = toBase64("static/images/question.svg");
+		String questionIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/question.png";
 		return """
 				<tr><td style="padding:0 24px;">
-					<img src="data:image/svg+xml;base64,%s" width="20" style="vertical-align:middle; margin-right:6px;">
+					<img src="%s" width="20" style="vertical-align:middle; margin-right:6px;">
 					<span style="font-size:22px; font-weight:bold;">%s</span>
 				</td></tr>
-			""".formatted(questionIcon, escapeHtml(questionTitle));
+			""".formatted(questionIconUrl, escapeHtml(questionTitle));
 	}
 
 	private static String instructionSection() {
@@ -126,28 +120,28 @@ public class MailTemplate {
 	}
 
 	private static String buttonSection(UUID userId) {
-		String usageIcon = toBase64("static/images/usage.svg");
-		String unsubscribeIcon = toBase64("static/images/unsubscription.svg");
+		String usageIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/usage.png";
+		String unsubscriptionIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/cancel.png";
 		return """
 				<tr><td align="center" style="padding:36px 24px;">
 					<table cellpadding="0" cellspacing="0">
 						<tr>
 							<td align="center" style="padding:0 28px;">
 								<a href="#" target="_blank" style="text-decoration:none;">
-									<img src="data:image/svg+xml;base64,%s" width="24" style="display:block; margin-bottom:10px;">
+									<img src="%s" width="24" style="display:block; margin-bottom:10px;">
 									<div style="font-size:14px; color:#333;">이용방법</div>
 								</a>
 							</td>
 							<td align="center" style="padding:0 28px;">
 								<a href="https://www.csalgo.co.kr/unsubscription?userId=%s" target="_blank" style="text-decoration:none;">
-									<img src="data:image/svg+xml;base64,%s" width="24" style="display:block; margin-bottom:10px;">
+									<img src="%s" width="24" style="display:block; margin-bottom:10px;">
 									<div style="font-size:14px; color:#333;">구독 해지</div>
 								</a>
 							</td>
 						</tr>
 					</table>
 				</td></tr>
-			""".formatted(usageIcon, userId.toString(), unsubscribeIcon);
+			""".formatted(usageIconUrl, userId.toString(), unsubscriptionIconUrl);
 	}
 
 	private static String footerSection() {
@@ -159,42 +153,42 @@ public class MailTemplate {
 	}
 
 	private static String feedbackQuestionSection(String questionTitle) {
-		String questionIcon = toBase64("static/images/question.svg");
+		String questionIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/question.png";
 		return """
 				<tr><td style="padding:24px 24px 0;">
-					<img src="data:image/svg+xml;base64,%s" width="20" style="vertical-align:middle; margin-right:6px;">
+					<img src="%s" width="20" style="vertical-align:middle; margin-right:6px;">
 					<span style="font-size:18px; font-weight:bold;">CS-ALGO가 질문한 내용이에요!</span>
 				</td></tr>
 				<tr><td style="padding:8px 24px 0; font-size:15px; color:#333; line-height:1.6;">
 					%s
 				</td></tr>
-			""".formatted(questionIcon, escapeHtml(questionTitle));
+			""".formatted(questionIconUrl, escapeHtml(questionTitle));
 	}
 
 	private static String feedbackUserAnswerSection(String username, String userAnswer) {
-		String responseIcon = toBase64("static/images/response.svg");
+		String responseIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/response.png";
 		return """
 				<tr><td style="padding:32px 24px 0;">
-					<img src="data:image/svg+xml;base64,%s" width="20" style="vertical-align:middle; margin-right:6px;">
+					<img src="%s" width="20" style="vertical-align:middle; margin-right:6px;">
 					<span style="font-size:18px; font-weight:bold;">%s님이 이렇게 답변했어요!</span>
 				</td></tr>
 				<tr><td style="padding:8px 24px 0; font-size:15px; color:#333; line-height:1.6;">
 					%s
 				</td></tr>
-			""".formatted(responseIcon, escapeHtml(username), escapeHtml(userAnswer));
+			""".formatted(responseIconUrl, escapeHtml(username), escapeHtml(userAnswer));
 	}
 
 	private static String feedbackModelAnswerSection(String modelAnswer) {
-		String solutionIcon = toBase64("static/images/solution.svg");
+		String solutionIconUrl = "https://csalgo-bucket.s3.ap-northeast-2.amazonaws.com/image/solution.png";
 		return """
 				<tr><td style="padding:32px 24px 0;">
-					<img src="data:image/svg+xml;base64,%s" width="20" style="vertical-align:middle; margin-right:6px;">
+					<img src="%s" width="20" style="vertical-align:middle; margin-right:6px;">
 					<span style="font-size:18px; font-weight:bold;">이렇게 답변해보면 어떨까요? (by CS-ALGO)</span>
 				</td></tr>
 				<tr><td style="padding:8px 24px 36px; font-size:15px; color:#333; line-height:1.6;">
 					%s
 				</td></tr>
-			""".formatted(solutionIcon, escapeHtml(modelAnswer));
+			""".formatted(solutionIconUrl, escapeHtml(modelAnswer));
 	}
 
 	private static String escapeHtml(String input) {
@@ -205,14 +199,5 @@ public class MailTemplate {
 			.replace("<", "&lt;")
 			.replace(">", "&gt;")
 			.replace("\"", "&quot;");
-	}
-
-	private static String toBase64(String pathInClassPath) {
-		try (InputStream input = new ClassPathResource(pathInClassPath).getInputStream()) {
-			byte[] bytes = input.readAllBytes();
-			return Base64.getEncoder().encodeToString(bytes);
-		} catch (IOException e) {
-			throw new IllegalStateException("이미지 인코딩 실패: " + pathInClassPath, e);
-		}
 	}
 }
