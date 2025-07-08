217e30f22b25d6cac8769a495342ea1370dbe1d4
Jinwoo Lee <jinlee1703@gmail.com>
2025-07-09 02:15:23 +0900

    feat: redis 기반 인증코드 저장소 구현 #119

---
+++ src/main/java/kr/co/csalgo/infrastructure/auth/repository/RedisVerificationCodeRepository.java
@@ -0,0 +1,37 @@
+package kr.co.csalgo.infrastructure.auth.repository;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import kr.co.csalgo.domain.auth.repository.VerificationCodeRepository;
+import kr.co.csalgo.domain.auth.type.VerificationCodeType;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class RedisVerificationCodeRepository implements VerificationCodeRepository {
+	private final StringRedisTemplate redisTemplate;
+	private final long expireTime;
+
+	@Override
+	public void create(String email, String code, VerificationCodeType verificationCodeType) {
+		String key = generateKey(verificationCodeType, email);
+		redisTemplate.opsForValue().set(key, code, expireTime, TimeUnit.SECONDS);
+	}
+
+	@Override
+	public boolean verify(String email, String code, VerificationCodeType verificationCodeType) {
+		String key = generateKey(verificationCodeType, email);
+		String storedCode = redisTemplate.opsForValue().get(key);
+
+		if (storedCode != null && storedCode.equals(code)) {
+			redisTemplate.delete(key);
+			return true;
+		}
+		return false;
+	}
+
+	private String generateKey(VerificationCodeType verificationCodeType, String email) {
+		return verificationCodeType + "::" + email;
+	}
+}
