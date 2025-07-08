3e99fa001736eb1206f9d8bcc48ef226452a12af
Jinwoo Lee <jinlee1703@gmail.com>
2025-07-09 02:19:28 +0900

    feat: 인증코드 저장소를 redis 기반으로 교체 #119

--- src/main/java/kr/co/csalgo/infrastructure/auth/config/VerificationCodeConfig.java
+++ src/main/java/kr/co/csalgo/infrastructure/auth/config/VerificationCodeConfig.java
@@ -3,9 +3,10 @@ package kr.co.csalgo.infrastructure.auth.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import kr.co.csalgo.domain.auth.repository.VerificationCodeRepository;
-import kr.co.csalgo.infrastructure.auth.repository.ConcurrentMapVerificationCodeRepository;
+import kr.co.csalgo.infrastructure.auth.repository.RedisVerificationCodeRepository;
 
 @Configuration
 public class VerificationCodeConfig {
@@ -14,7 +15,10 @@ public class VerificationCodeConfig {
 	private long codeExpiration;
 
 	@Bean
-	public VerificationCodeRepository verificationCodeRepository() {
-		return new ConcurrentMapVerificationCodeRepository(codeExpiration);
+	public VerificationCodeRepository verificationCodeRepository(StringRedisTemplate redisTemplate) {
+		return new RedisVerificationCodeRepository(
+			redisTemplate,
+			codeExpiration
+		);
 	}
 }
