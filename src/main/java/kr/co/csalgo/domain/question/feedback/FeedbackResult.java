b48926e8d468f2972282f731a63a86c9484c8ff3
Jinwoo Lee <jinlee1703@gmail.com>
2025-07-11 02:32:50 +0900

    feat: 피드백 메일 템플릿에 유사도 및 가이드 문구 인자 전달 #118

--- src/main/java/kr/co/csalgo/domain/question/feedback/FeedbackResult.java
+++ src/main/java/kr/co/csalgo/domain/question/feedback/FeedbackResult.java
@@ -9,11 +9,15 @@ import lombok.NoArgsConstructor;
 public class FeedbackResult {
 	private String responseContent;
 	private String questionSolution;
+	private double similarity;
+	private String guideMessage;
 
 	@Builder
-	public FeedbackResult(String responseContent, String questionSolution) {
+	public FeedbackResult(String responseContent, String questionSolution, double similarity, String guideMessage) {
 		this.responseContent = responseContent;
 		this.questionSolution = questionSolution;
+		this.similarity = similarity;
+		this.guideMessage = guideMessage;
 	}
 }
 
