b958d88b1b91095d0a21aacf03f0f288a4c3e5ac
Jinwoo Lee <jinlee1703@gmail.com>
2025-07-11 14:50:08 +0900

    feat: Redis를 활용하도록 ShedLockConfig 정의 #116

---
+++ src/main/java/kr/co/csalgo/config/ShedLockConfig.java
@@ -0,0 +1,18 @@
+package kr.co.csalgo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+
+@Configuration
+@EnableSchedulerLock(defaultLockAtMostFor = "1m")
+public class ShedLockConfig {
+	@Bean
+	public LockProvider lockProvider(RedisConnectionFactory redisConnectionFactory) {
+		return new RedisLockProvider(redisConnectionFactory);
+	}
+}
