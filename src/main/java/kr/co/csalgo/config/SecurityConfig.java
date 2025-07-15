c0a9db166038b9099d5210bf7d297363a2095a59
wlstj <22020306jinseoyeon@gmail.com>
2025-07-15 13:16:12 +0900

    feat: securityconfig 필터 추가 #138

--- src/main/java/kr/co/csalgo/config/SecurityConfig.java
+++ src/main/java/kr/co/csalgo/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package kr.co.csalgo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +15,15 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(authorize -> authorize
+				.anyRequest().permitAll()
+			);
+		return httpSecurity.build();
 	}
 }
