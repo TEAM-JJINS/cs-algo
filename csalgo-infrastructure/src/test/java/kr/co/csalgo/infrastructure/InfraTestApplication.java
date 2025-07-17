2fd394269846c060229a6dee568e074ba98e71d5
Jinwoo Lee <jinlee1703@gmail.com>
2025-07-17 23:53:14 +0900

    chore: infrastructure 모듈 테스트 코드 이동으로 인한 오류 해결 #148

---
+++ csalgo-infrastructure/src/test/java/kr/co/csalgo/infrastructure/InfraTestApplication.java
@@ -0,0 +1,7 @@
+package kr.co.csalgo.infrastructure;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraTestApplication {
+}
--- csalgo-infrastructure/src/test/java/kr/co/csalgo/infrastructure/actuator/HealthCheckTest.java
+++ csalgo-infrastructure/src/test/java/kr/co/csalgo/infrastructure/actuator/HealthCheckTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import redis.embedded.RedisServer;
@@ -20,6 +21,7 @@ import redis.embedded.RedisServer;
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@TestPropertySource(properties = "verification.code.expiration=300000")
 class HealthCheckTest {
 
 	private static RedisServer redisServer;
