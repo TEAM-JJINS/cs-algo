e3ff0b33326fd88429e971ef231608678220ac19
Jinwoo Lee <jinlee1703@gmail.com>
2025-07-18 20:25:42 +0900

    fix: server 모듈 main 임시 비활성화 처리 #148

--- csalgo-server/src/main/java/kr/co/csalgo/CsAlgoServerApplication.java
+++ csalgo-server/src/main/java/kr/co/csalgo/CsAlgoServerApplication.java
@@ -1,9 +1,8 @@
 package kr.co.csalgo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
 public class CsAlgoServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CsAlgoServerApplication.class, args);
