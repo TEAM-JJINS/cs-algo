66669825512553bd8e8f9055bf9f50068db28575
wlstj <22020306jinseoyeon@gmail.com>
2025-07-18 22:29:05 +0900

    refactor: page에서 list로 반환 수정 #139

--- csalgo-application/src/main/java/kr/co/csalgo/application/problem/usecase/GetQuestionUseCase.java
+++ csalgo-application/src/main/java/kr/co/csalgo/application/problem/usecase/GetQuestionUseCase.java
@@ -1,6 +1,7 @@
 package kr.co.csalgo.application.problem.usecase;
 
-import org.springframework.data.domain.Page;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import kr.co.csalgo.application.problem.dto.QuestionDto;
@@ -14,18 +15,13 @@ import lombok.extern.slf4j.Slf4j;
 public class GetQuestionUseCase {
 	private final QuestionService questionService;
 
-<<<<<<< HEAD
 	public List<QuestionDto.Response> getQuestionListWithPaging(int page, int size) {
 		List<QuestionDto.Response> questions = questionService.list(page - 1, size).stream()
 			.map(QuestionDto.Response::of)
 			.toList();
 		log.info("[문제 리스트 조회 완료] count:{}", questions.size());
 		return questions;
-=======
-	public Page<QuestionDto.Response> getQuestionListWithPaging(int page, int size) {
-		return questionService.list(page, size)
-			.map(QuestionDto.Response::of);
->>>>>>> 8309530 (feat: 목록 조회 시 페이징 #139)
+
 	}
 
 }
