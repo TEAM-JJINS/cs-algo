77cce073447e0238c6986916c546fa5848ad7ba6
wlstj <22020306jinseoyeon@gmail.com>
2025-07-18 20:18:09 +0900

    feat: 목록 조회 시 페이징 #139

--- csalgo-application/src/main/java/kr/co/csalgo/application/problem/usecase/GetQuestionUseCase.java
+++ csalgo-application/src/main/java/kr/co/csalgo/application/problem/usecase/GetQuestionUseCase.java
@@ -1,7 +1,6 @@
 package kr.co.csalgo.application.problem.usecase;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import kr.co.csalgo.application.problem.dto.QuestionDto;
@@ -15,12 +14,18 @@ import lombok.extern.slf4j.Slf4j;
 public class GetQuestionUseCase {
 	private final QuestionService questionService;
 
+<<<<<<< HEAD
 	public List<QuestionDto.Response> getQuestionListWithPaging(int page, int size) {
 		List<QuestionDto.Response> questions = questionService.list(page - 1, size).stream()
 			.map(QuestionDto.Response::of)
 			.toList();
 		log.info("[문제 리스트 조회 완료] count:{}", questions.size());
 		return questions;
+=======
+	public Page<QuestionDto.Response> getQuestionListWithPaging(int page, int size) {
+		return questionService.list(page, size)
+			.map(QuestionDto.Response::of);
+>>>>>>> 8309530 (feat: 목록 조회 시 페이징 #139)
 	}
 
 }
