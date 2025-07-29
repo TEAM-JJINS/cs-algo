fba4de6ec77400dc6558dbba9b8ff06145dc2add
Jinwoo Lee <jinlee1703@gmail.com>
2025-07-29 14:06:24 +0900

    feat: 네이버 클라우드 환경 사용을 위한 API키 변수 작성 #162

---
+++ csalgo-iac/variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  description = "네이버 클라우드 플랫폼 API Access Key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "네이버 클라우드 플랫폼 API Secret Key"
+  type        = string
+}
