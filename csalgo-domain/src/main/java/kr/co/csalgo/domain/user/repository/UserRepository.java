1c766fe61e976b877c084571e98a4bbe1c71312c
wlstj <22020306jinseoyeon@gmail.com>
2025-07-29 15:41:03 +0900

    feat: userservice pageable로 조회 #143

--- csalgo-domain/src/main/java/kr/co/csalgo/domain/user/repository/UserRepository.java
+++ csalgo-domain/src/main/java/kr/co/csalgo/domain/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package kr.co.csalgo.domain.user.repository;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +12,8 @@ import kr.co.csalgo.domain.user.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	Page<User> findAll(Pageable pageable);
+
 	boolean existsByEmail(String email);
 
 	Optional<User> findByUuid(UUID uuid);
