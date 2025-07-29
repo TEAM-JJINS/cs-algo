24a55b0bf30f284ec543fec713e35a985ffb6731
wlstj <22020306jinseoyeon@gmail.com>
2025-07-29 16:12:32 +0900

    test: 사용자 목록 조회 API test #143

---
+++ csalgo-server/src/test/java/kr/co/csalgo/server/user/UserControllerTest.java
@@ -0,0 +1,63 @@
+package kr.co.csalgo.server.user;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kr.co.csalgo.application.user.dto.UserDto;
+import kr.co.csalgo.application.user.usecase.GetUserUseCase;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class UserControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockitoBean
+	private GetUserUseCase getUserUseCase;
+
+	@Autowired
+	private ObjectMapper mapper;
+
+	@Test
+	@DisplayName("사용자 목록 조회 성공 시 200 OK 반환")
+	void testGetUserListSuccess() throws Exception {
+		List<UserDto.Response> users = List.of(
+			UserDto.Response.builder()
+				.email("user1@example.com")
+				.uuid(UUID.randomUUID())
+				.build(),
+			UserDto.Response.builder()
+				.email("user2@example.com")
+				.uuid(UUID.randomUUID())
+				.build()
+		);
+
+		when(getUserUseCase.getUserListWithPaging(1, 2)).thenReturn(users);
+
+		mockMvc.perform(get("/api/users")
+				.param("page", "1")
+				.param("size", "2"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$").isArray())
+			.andExpect(jsonPath("$.length()").value(2))
+			.andExpect(jsonPath("$[0].email").value("user1@example.com"))
+			.andExpect(jsonPath("$[1].email").value("user2@example.com"));
+	}
+
+}
