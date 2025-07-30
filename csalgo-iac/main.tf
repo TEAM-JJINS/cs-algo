981f3443a52cb3858dc5e7883d37763fc1ae09a4
Jinwoo Lee <jinlee1703@gmail.com>
2025-07-30 17:06:09 +0900

    feat: server 모듈 호출 #162

--- csalgo-iac/main.tf
+++ csalgo-iac/main.tf
@@ -9,3 +9,20 @@ module "mysql" {
 module "redis" {
   source = "./modules/redis"
 }
+
+module "server" {
+  source = "./modules/server"
+
+  image         = "csalgo.kr.ncr.ntruss.com/csalgo-server:latest"
+
+  mail_host     = var.mail_host
+  mail_port     = var.mail_port
+  mail_username = var.mail_username
+  mail_password = var.mail_password
+
+  db_url        = "jdbc:mysql://${module.mysql.mysql_host}:3306/${module.mysql.mysql_database}"
+  db_username   = module.mysql.mysql_username
+  db_password   = module.mysql.mysql_password
+
+  sentry_dsn    = var.sentry_dsn
+}
