d87e46b526544a3e8384ca09f2d7b61b4eb78c64
Jinwoo Lee <jinlee1703@gmail.com>
2025-07-31 14:27:00 +0900

    feat: web 모듈을 위한 변수 정의 #162

---
+++ csalgo-iac/modules/web/variables.tf
@@ -0,0 +1,9 @@
+variable "image" {
+  description = "웹 애플리케이션의 Docker 이미지"
+  type        = string
+}
+
+variable "external_api_base_url" {
+  description = "API 호출을 위한 외부 API의 기본 URL"
+  type        = string
+}
