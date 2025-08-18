ff124bfc6daa2c07795dd4523d52610ba918e74a
Jinwoo Lee <jinlee1703@gmail.com>
2025-08-19 03:15:23 +0900

    feat: JWT 기반 인증 필터 구현 #135

---
+++ csalgo-infrastructure/src/main/java/kr/co/csalgo/infrastructure/security/JwtAuthenticationFilter.java
@@ -0,0 +1,29 @@
+package kr.co.csalgo.infrastructure.security;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	private final JwtProvider jwtProvider;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws
+		ServletException, IOException {
+		String token = jwtProvider.resolveToken(request);
+		if (token != null && !jwtProvider.validateToken(token)) {
+			Authentication auth = jwtProvider.getAuthentication(token);
+			SecurityContextHolder.getContext().setAuthentication(auth);
+		}
+		filterChain.doFilter(request, response);
+	}
+}
