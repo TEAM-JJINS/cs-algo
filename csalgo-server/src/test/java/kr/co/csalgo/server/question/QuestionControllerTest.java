e3b4652aad005453d447073b7d4373c58f6d2d07
Jinwoo Lee <jinlee1703@gmail.com>
2025-08-19 04:32:11 +0900

    test: 관리자 권한 API 테스트를 위한 MockUser 설정 #135

--- csalgo-server/src/test/java/kr/co/csalgo/server/question/QuestionControllerTest.java
+++ csalgo-server/src/test/java/kr/co/csalgo/server/question/QuestionControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,6 +36,7 @@ import kr.co.csalgo.common.message.MessageCode;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@WithMockUser(username = "admin@csalgo.co.kr", roles = {"ADMIN"})
 class QuestionControllerTest {
 
 	@Autowired
