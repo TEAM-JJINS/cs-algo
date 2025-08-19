66b02246eb0de358fbe67ef2fd1a0906739f74d4
Jinwoo Lee <jinlee1703@gmail.com>
2025-08-19 14:12:11 +0900

    feat: 어드민 로그인 페이지 UI 구현 및 WebController 작성 #130

---
+++ csalgo-web/src/main/java/kr/co/csalgo/web/admin/controller/AdminWebController.java
@@ -0,0 +1,17 @@
+package kr.co.csalgo.web.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminWebController {
+	@GetMapping("/login")
+	public String mainPage() {
+		return "admin/login";
+	}
+}
