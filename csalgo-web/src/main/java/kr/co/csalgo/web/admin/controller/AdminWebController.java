b8cd3ce49e0063092f1258f17b8f3836b05d67fd
Jinwoo Lee <jinlee1703@gmail.com>
2025-08-19 16:00:33 +0900

    feat: 관리자 로그인 API 연동 #130

--- csalgo-web/src/main/java/kr/co/csalgo/web/admin/controller/AdminWebController.java
+++ csalgo-web/src/main/java/kr/co/csalgo/web/admin/controller/AdminWebController.java
@@ -1,15 +1,21 @@
 package kr.co.csalgo.web.admin.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import kr.co.csalgo.web.admin.service.AdminService;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminWebController {
+	private final AdminService adminService;
+
 	@GetMapping("/login")
 	public String mainPage() {
 		return "admin/login";
@@ -18,5 +24,15 @@ public class AdminWebController {
 	@GetMapping("/dashboard")
 	public String dashboard() {
 		return "admin/dashboard";
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestParam String email, @RequestParam String password) {
+		return adminService.login(email, password); // JSON 반환
+	}
+
+	@PostMapping("/refresh")
+	public ResponseEntity<?> refresh(@RequestParam String refreshToken) {
+		return adminService.refresh(refreshToken);
 	}
 }
