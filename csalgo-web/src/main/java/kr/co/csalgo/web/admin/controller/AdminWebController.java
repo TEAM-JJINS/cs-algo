fc854864d8d8c6c13ff00a788d83f765d12ff6e7
Jinwoo Lee <jinlee1703@gmail.com>
2025-08-20 15:04:21 +0900

    feat: 관리자 대시보드 회원관리 페이지 UI 구현 #133

--- csalgo-web/src/main/java/kr/co/csalgo/web/admin/controller/AdminWebController.java
+++ csalgo-web/src/main/java/kr/co/csalgo/web/admin/controller/AdminWebController.java
@@ -1,5 +1,8 @@
 package kr.co.csalgo.web.admin.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,6 +29,15 @@ public class AdminWebController {
 	public String dashboard(Model model) {
 		model.addAttribute("activeMenu", "dashboard");
 		return "admin/dashboard/index";
+	}
+
+	@GetMapping("/dashboard/members")
+	public String members(@PageableDefault(size = 10) Pageable pageable, Model model) {
+		// Page<Member> members = memberService.findAll(pageable);
+		Page<?> members = Page.empty(pageable);
+		model.addAttribute("members", members);
+		model.addAttribute("activeMenu", "members");
+		return "admin/dashboard/members";
 	}
 
 	@PostMapping("/login")
