68ac4b224c18d6e3ce1934153f83fb09e4da4c1a
Jinwoo Lee <jinlee1703@gmail.com>
2025-08-20 15:22:44 +0900

    feat: 사용자 조회 API 호출을 위한 RestClient 메서드 작성 #133

--- csalgo-web/src/main/java/kr/co/csalgo/web/admin/client/AdminRestClient.java
+++ csalgo-web/src/main/java/kr/co/csalgo/web/admin/client/AdminRestClient.java
@@ -6,6 +6,7 @@ import org.springframework.web.client.RestClient;
 
 import kr.co.csalgo.web.admin.dto.AdminLoginDto;
 import kr.co.csalgo.web.admin.dto.AdminRefreshDto;
+import kr.co.csalgo.web.admin.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -27,5 +28,19 @@ public class AdminRestClient {
 			.body(body)
 			.retrieve()
 			.toEntity(AdminRefreshDto.Response.class);
+	}
+
+	public ResponseEntity<?> getUser(int id) {
+		return restClient.get()
+			.uri("/users/{id}", id)
+			.retrieve()
+			.toEntity(UserDto.Response.class);
+	}
+
+	public ResponseEntity<?> getUserList(int page, int size) {
+		return restClient.get()
+			.uri("/users?page={page}&size={size}", page, size)
+			.retrieve()
+			.toEntity(UserDto.Response.class);
 	}
 }
