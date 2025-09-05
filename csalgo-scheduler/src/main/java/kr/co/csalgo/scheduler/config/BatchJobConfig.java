6e4a3a4f1ab1ac4d4559d78bda318dd15da351fa
wlstj <22020306jinseoyeon@gmail.com>
2025-09-05 17:35:51 +0900

    feat: 문제 발송 job 용 tackletstep/chunkstep 추가 #205

--- csalgo-scheduler/src/main/java/kr/co/csalgo/scheduler/config/BatchJobConfig.java
+++ csalgo-scheduler/src/main/java/kr/co/csalgo/scheduler/config/BatchJobConfig.java
@@ -2,14 +2,19 @@ package kr.co.csalgo.scheduler.config;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import kr.co.csalgo.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,6 +33,30 @@ public class BatchJobConfig {
 			.listener(jobExecutionListener)
 			.start(problemTaskletStep(jobRepository, tx))
 			.next(problemChunkStep(jobRepository, tx))
+			.build();
+	}
+
+	@Bean
+	@JobScope
+	public Step problemTaskletStep(JobRepository jobRepository, PlatformTransactionManager tx) {
+		return new StepBuilder("problemTaskletStep", jobRepository)
+			.listener(stepExecutionListener)
+			.tasklet((contribution, chunkContext) -> {
+				log.info("[문제 전송] 문제 전송 job 실행");
+				return RepeatStatus.FINISHED;
+			}, tx)
+			.build();
+	}
+
+	@Bean
+	@JobScope
+	public Step problemChunkStep(JobRepository jobRepository, PlatformTransactionManager batchTransactionManager) {
+		return new StepBuilder("problemChunkStep", jobRepository)
+			.listener(stepExecutionListener)
+			.<User, User>chunk(10, batchTransactionManager)
+			.reader(problemReader())
+			.processor(problemProcessor())
+			.writer(problemWriter())
 			.build();
 	}
 
