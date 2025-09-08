027b0c235b179d5a99a6a50de59c8709af1beaf5
wlstj <22020306jinseoyeon@gmail.com>
2025-09-08 13:37:33 +0900

    chore: spring-retry 라이브러리 추가 #205

--- csalgo-scheduler/src/main/java/kr/co/csalgo/CsAlgoSchedulerApplication.java
+++ csalgo-scheduler/src/main/java/kr/co/csalgo/CsAlgoSchedulerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableRetry
 @EnableAsync
 @EnableScheduling
 @EnableJpaAuditing
