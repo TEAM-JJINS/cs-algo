d6ce93171f3cc16ed6b4b4e85039ebfc0200a57a
wlstj <22020306jinseoyeon@gmail.com>
2025-09-08 23:33:31 +0900

    refactor: retry config 상수 분리 #205

--- csalgo-scheduler/src/main/java/kr/co/csalgo/scheduler/config/RetryConfig.java
+++ csalgo-scheduler/src/main/java/kr/co/csalgo/scheduler/config/RetryConfig.java
@@ -1,21 +1,34 @@
 package kr.co.csalgo.scheduler.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 public class RetryConfig {
+
+	private static final int RETRY_COUNT = 3;
+	private static final long BACK_OFF_DELAY = 1000L;
+
 	@Bean
 	public RetryTemplate retryTemplate() {
-		ExponentialBackOffPolicy backoff = new ExponentialBackOffPolicy();
-		backoff.setInitialInterval(2000);
-		backoff.setMultiplier(2.0);
-		backoff.setMaxInterval(15000);
+		SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+		retryPolicy.setMaxAttempts(RETRY_COUNT);
 
-		RetryTemplate retry = new RetryTemplate();
-		retry.setBackOffPolicy(backoff);
-		return retry;
+		FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+		backOffPolicy.setBackOffPeriod(BACK_OFF_DELAY);
+
+		List<Class<? extends Throwable>> retryOn = List.of(RuntimeException.class);
+
+		return RetryTemplate.builder()
+			.customPolicy(retryPolicy)
+			.customBackoff(backOffPolicy)
+			.retryOn(retryOn)
+			.build();
 	}
 }
+
