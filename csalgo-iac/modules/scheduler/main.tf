67cc0e5ccb135171c9df7ee71e1f776bca23cf05
Jinwoo Lee <jinlee1703@gmail.com>
2025-09-21 22:09:44 +0900

    fix: Scheduler 모듈 MemoryPressure 방지 리소스 제한 추가 #234

--- csalgo-iac/modules/scheduler/main.tf
+++ csalgo-iac/modules/scheduler/main.tf
@@ -177,6 +177,17 @@ resource "kubernetes_deployment" "csalgo_scheduler" {
           port {
             container_port = 8080
           }
+
+          resources {
+            requests = {
+              cpu    = "250m"
+              memory = "512Mi"
+            }
+            limits = {
+              cpu    = "500m"
+              memory = "1Gi"
+            }
+          }
         }
       }
     }
