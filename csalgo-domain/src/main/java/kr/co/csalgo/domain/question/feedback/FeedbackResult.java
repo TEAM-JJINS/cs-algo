bd080278547efc0d22982b26f94ccf74e90f1d4d
wlstj <22020306jinseoyeon@gmail.com>
2025-09-22 19:47:57 +0900

    refactor: feedbackresult 속성 추가 #211

--- csalgo-domain/src/main/java/kr/co/csalgo/domain/question/feedback/FeedbackResult.java
+++ csalgo-domain/src/main/java/kr/co/csalgo/domain/question/feedback/FeedbackResult.java
@@ -1,5 +1,7 @@
 package kr.co.csalgo.domain.question.feedback;
 
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,17 +9,21 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class FeedbackResult {
-	private String responseContent;
-	private String questionSolution;
+	private String summary;
+	private List<String> strengths;
+	private List<String> improvements;
+	private List<String> suggestions;
+	private List<String> learningTips;
 	private double similarity;
-	private String guideMessage;
 
 	@Builder
-	public FeedbackResult(String responseContent, String questionSolution, double similarity, String guideMessage) {
-		this.responseContent = responseContent;
-		this.questionSolution = questionSolution;
+	public FeedbackResult(String summary, List<String> strengths, List<String> improvements,
+		List<String> suggestions, List<String> learningTips, double similarity) {
+		this.summary = summary;
+		this.strengths = strengths;
+		this.improvements = improvements;
+		this.suggestions = suggestions;
+		this.learningTips = learningTips;
 		this.similarity = similarity;
-		this.guideMessage = guideMessage;
 	}
 }
-
