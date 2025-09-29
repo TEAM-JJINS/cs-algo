37fed30d8452dd38d236bc0700c0aa73dc074daa
Jinwoo Lee <jinlee1703@gmail.com>
2025-09-30 02:59:58 +0900

    feat: mysql 모듈 호출부 제거 #246

--- csalgo-iac/main.tf
+++ csalgo-iac/main.tf
@@ -1,11 +1,3 @@
-module "mysql" {
-  source        = "./modules/mysql"
-  root_password = var.mysql_root_password
-  db_name       = var.mysql_db_name
-  username      = var.mysql_user
-  password      = var.mysql_password
-}
-
 module "redis" {
   source = "./modules/redis"
 }
